2684f94a3b7221322a6bb5ad16dbdd65e8453f54
misterclayt0n <daviarantes13@gmail.com>
2024-04-29 10:24:06 -0300

    add encoding json with structs

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/misterclayt0n/greenlight/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,11 +14,23 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIdParam(r)
-
-	if err != nil || id < 1 {
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJson(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
